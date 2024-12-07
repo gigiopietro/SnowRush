85967b7dd2168f00728cafa3f6b02073de2ef375
Giovanna <giovannahenrichs78@gmail.com>
2024-12-07 20:28:09 -0300

    Mudando a fonte;

--- source/Score.py
+++ source/Score.py
@@ -75,7 +75,7 @@ class Score:
             pygame.display.flip()
 
     def score_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-        text_font: Font = pygame.font.SysFont(name="PromptFont", size=text_size)
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
         text_rect: Rect = text_surf.get_rect(center=text_center_pos)
         self.display_surface.blit(source=text_surf, dest=text_rect)
